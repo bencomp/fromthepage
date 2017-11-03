2e123d26e4fe89598c3ba536545f5e4cbb97809e
Ben Companjen <ben@companjen.name>
2017-11-03 17:00:32 +0100

    Put OSD back in app/assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,4 @@
 Rails.application.config.assets.precompile += %w( bookreader.js )
 Rails.application.config.assets.precompile += %w( textdiff.js )
-Rails.application.config.assets.precompile += %w(vendor/assets/images/iiif-images/*)
 Rails.application.config.assets.precompile += %w( openseadragon.js )
 Rails.application.config.assets.precompile += %w( datepicker/datepicker.js)
