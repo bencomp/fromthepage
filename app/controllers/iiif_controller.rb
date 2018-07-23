8d346cfc5d3df18b4cb47233d5b673052e4ec8ad
Ben Companjen <ben@companjen.name>
2018-07-23 16:24:31 +0200

    Don't add a region to the canvas URI for annotations

--- app/controllers/iiif_controller.rb
+++ app/controllers/iiif_controller.rb
@@ -476,7 +476,7 @@ private
   end
 
   def region_from_page(page)
-    canvas_id_from_page(page) + "#xywh=0,0,#{(page.base_width || page.sc_canvas.width)},#{(page.base_height || page.sc_canvas.height)}"
+    canvas_id_from_page(page) #+ "#xywh=0,0,#{(page.base_width || page.sc_canvas.width)},#{(page.base_height || page.sc_canvas.height)}"
   end
 
   def iiif_create_image_resource(page)
